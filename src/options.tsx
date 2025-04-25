462f65164c360ae25dde250051a6ce2822f6f3d3
Nicholas Jones <njonesgrad@gmail.com>
2025-04-25 19:54:56 +0200

    Delete Comments

--- src/options.tsx
+++ src/options.tsx
@@ -1,26 +1,3 @@
-/*
- *   Copyright (c) 2023 R3BL LLC
- *   All rights reserved.
- *
- *   Permission is hereby granted, free of charge, to any person obtaining a copy
- *   of this software and associated documentation files (the "Software"), to deal
- *   in the Software without restriction, including without limitation the rights
- *   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- *   copies of the Software, and to permit persons to whom the Software is
- *   furnished to do so, subject to the following conditions:
- *
- *   The above copyright notice and this permission notice shall be included in all
- *   copies or substantial portions of the Software.
- *
- *   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- *   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- *   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- *   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- *   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- *   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- *   SOFTWARE.
- */
-
 import React, { useEffect, useState } from "react"
 import { createRoot } from "react-dom/client"
 
@@ -30,8 +7,6 @@ const Options = () => {
   const [like, setLike] = useState<boolean>(false)
 
   useEffect(() => {
-    // Restores select box and checkbox state using the preferences
-    // stored in chrome.storage.
     chrome.storage.sync.get(
       {
         favoriteColor: "red",
@@ -45,14 +20,12 @@ const Options = () => {
   }, [])
 
   const saveOptions = () => {
-    // Saves options to chrome.storage.sync.
     chrome.storage.sync.set(
       {
         favoriteColor: color,
         likesColor: like,
       },
       () => {
-        // Update status to let user know options were saved.
         setStatus("Options saved.")
         const id = setTimeout(() => {
           setStatus("")
